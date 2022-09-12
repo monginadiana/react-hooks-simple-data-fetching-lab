663bde2c8b7810fd0a348244fee194e51f02c34b
monginadiana <dianahmongina2@gmail.com>
2022-09-12 12:22:32 +0300

    addition of the dog bread

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+
+
+import React, { useEffect, useState } from "react";
+
+function App(){
+    const [dogBread,setDogBread] = useState ([])
+    const [isLoaded, setIsLoaded] = useState(false);
+
+
+
+    useEffect (() =>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((image) => {
+            setDogBread(image.message)
+            setIsLoaded(true);
+        })
+    }, [])
+    if (!isLoaded) return <p>Loading...</p>;
+
+    return (
+    
+    <img src={dogBread.map((image1) => image1.image)} alt="A Random Dog" />
+      
+    )
+}
+
+export default App
