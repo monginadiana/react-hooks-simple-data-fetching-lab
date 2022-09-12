cc2b56e0a06abfe49c3226450958f3825e506b76
monginadiana <dianahmongina2@gmail.com>
2022-09-12 12:53:13 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,24 @@ import React, { useEffect, useState } from "react";
 
 function App(){
     const [dogBread,setDogBread] = useState ([])
-    const [isLoaded, setIsLoaded] = useState(false);
+    // const [isLoaded, setIsLoaded] = useState(false);
 
 
 
     useEffect (() =>{
         fetch("https://dog.ceo/api/breeds/image/random")
         .then((response) => response.json())
-        .then((image) => {
-            setDogBread(image.message)
-            setIsLoaded(true);
-        })
-    }, [])
-    if (!isLoaded) return <p>Loading...</p>;
+        .then((images) =>setDogBread(images));
+            // setIsLoaded(true);
+    },[]);
+    
+    if (!dogBread) return <p>Loading...</p>;
 
     return (
+    <>
+    <img src={dogBread} alt="A Random Dog" />
+    </>
     
-    <img src={dogBread.map((image1) => image1.image)} alt="A Random Dog" />
       
     )
 }
